4cf6a19b94271e6aefb63957054a38135eab9698
Luigi Indaco <luigindaco@gmail.com>
2022-10-14 13:32:10 +0200

    commit

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ async function mostraLibri() {
   let httpLibri = await fetch("https://striveschool-api.herokuapp.com/books");
   let tuttiLibri = await httpLibri.json();
   //^prendo i dati dalla fetch e li parso
-  document.querySelector("#container").innerHTML = "";
-  for (let i = 0; i < 3; i++) {
+  console.log(tuttiLibri);
+  let container = document.querySelector(".row");
+  container.innerHTML = "";
+  for (let i = 0; i < tuttiLibri.length; i++) {
     let libro = tuttiLibri[i];
-    document.querySelector("#container").innerHTML += `${tuttiLibri[i].title} ${tuttiLibri[i].img}${tuttiLibri[i].price}`;
-    console.log(libro);
+    container.innerHTML += `<div class="col col-lg-3 col-md-4 mb-5 align-self-stretch "> <div class="card ">
+    <img src=${libro.img} class="card-img-top " alt=${libro.title}>
+    <div class="card-body ">
+  <h5 class="card-title">${libro.title}</h5>
+  <p class="card-text">${libro.category}</p>
+ <a href="#" class="btn btn-primary">${libro.price}</a>
+   </div>
+ </div>`;
   }
-  //^ciclo che mi restituisce l'indice
+  //^ciclo che mi restituisce l'indice e crea le card
 }
+
 window.onload = () => {
-  document.querySelector("#container").innerHTML = "loading...";
+  document.querySelector(".row").innerHTML = "loading...";
   mostraLibri();
 };
